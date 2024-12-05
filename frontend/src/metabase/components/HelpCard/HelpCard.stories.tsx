3121825817250d40f4ddd8e92cb5c9084b847e51
Balerip <baleri.p@northeastern.edu>
2024-12-04 21:40:58 -0800

    adding new files

--- frontend/src/metabase/components/HelpCard/HelpCard.stories.tsx
+++ frontend/src/metabase/components/HelpCard/HelpCard.stories.tsx
@@ -11,13 +11,4 @@ const Template: StoryFn<HelpCardProps> = args => {
   return <HelpCard {...args} />;
 };
 
-export const Default = {
-  render: Template,
-
-  args: {
-    title: "Need help with anything?",
-    helpUrl: "https://metabase.com",
-    children:
-      "See our docs for step-by-step directions on how to do what you need.",
-  },
-};
+export const Default = null;
--- frontend/src/metabase/databases/components/DatabaseHelpCard/DatabaseHelpCard.tsx
+++ frontend/src/metabase/databases/components/DatabaseHelpCard/DatabaseHelpCard.tsx
@@ -11,30 +11,6 @@ export interface DatabaseHelpCardProps {
 
 export const DatabaseHelpCard = ({
   className,
-}: DatabaseHelpCardProps): JSX.Element => {
-  const docsUrl = useSelector(state =>
-    // eslint-disable-next-line no-unconditional-metabase-links-render -- Metabase setup
-    getDocsUrl(state, { page: "databases/connecting" }),
-  );
-  const isHosted = useSelector(state => getSetting(state, "is-hosted?"));
-
-  return (
-    <HelpCard
-      title={t`Need help connecting?`}
-      className={className}
-      isFullyClickable={!isHosted}
-      helpUrl={docsUrl}
-    >
-      <p>{t`See our docs for step-by-step directions on how to connect your database.`}</p>
-      {isHosted && (
-        <p>
-          {jt`Docs weren't enough? ${(
-            <ExternalLink key="link" href="https://www.metabase.com/help/cloud">
-              {t`Write us.`}
-            </ExternalLink>
-          )}`}
-        </p>
-      )}
-    </HelpCard>
-  );
+}: DatabaseHelpCardProps): JSX.Element | null => {
+  return null;
 };
